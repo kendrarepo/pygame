afe99160abba9fca966b606bc4c26284c8d6d10d
Kendra Repo <kendrarepo@Kendras-MacBook-Pro.local>
2016-11-20 08:26:58 -0500

    Game Screen

--- game.py
+++ game.py
@@ -3,3 +3,15 @@ from pygame import *
 from pygame.locals import *
 from pygame.sprite import *
 pygame.init(); # required
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+white = (255,255,255) # global colors
+black = (0,0,0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+gameDisplay = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #initialize with a tuple
+pygame.display.set_caption("Diag Squirrel Dodger") # title
